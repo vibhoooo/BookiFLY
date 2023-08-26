cfce71605f67ff69cacfc8872b7da0eec920e156
Vibhoo Srivastava <cse21105@iiitkalyani.ac.in>
2023-08-26 12:47:59 +0530

    implemeted user routes

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const validateTokenHandlerUser = require("../middlewares/validateTokenHandlerUser");
 const { signupUser } = require("../controllers/userControllers");
 const { loginUser } = require("../controllers/userControllers");
+const { selectSourceDestination } = require("../controllers/userControllers");
+const { selectFlight } = require("../controllers/userControllers");
+const { selectSeat } = require("../controllers/userControllers");
 router.route(
 	"/signup"
 ).post(
@@ -12,5 +15,23 @@ router.route(
 	"/login"
 ).post(
 	loginUser
+);
+router.route(
+	"/selectSD"
+).post(
+	validateTokenHandlerUser,
+	selectSourceDestination
+);
+router.route(
+	"/selectFlight"
+).post(
+	validateTokenHandlerUser,
+	selectFlight
+);
+router.route(
+	"/selectSeat"
+).post(
+	validateTokenHandlerUser,
+	selectSeat
 );
 module.exports = router;
